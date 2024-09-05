abdaf5a62b55d4aa2bbcc661ab7dccb77f6c9b15
Tomek <klingaroo@gmail.com>
2024-09-05 15:54:54 -0400

    init db_worker

---
+++ tms/db_worker.py
@@ -0,0 +1,41 @@
+from sqlalchemy import select, func
+from sqlalchemy.exc import NoResultFound
+
+from tms.tms_engine import get_engine, get_session
+from tms.models import Objects, Departments, Roles, RolesTypes
+
+
+def temp(engine):
+    with get_session(engine) as session:
+        rows = session.execute(
+            select(Roles.Role, Roles.RoleID).where(Roles.RoleTypeID == 1)
+        ).all()
+        for r in rows:
+            # print(type(r))
+            # print(r)
+            # break
+            print(f"{r.Role} = {r.RoleID}")
+
+
+def object_by_dept_count(engine, dept_id: int):
+    with get_session(engine) as session:
+        row_count = session.scalar(
+            select(func.count())
+            .select_from(Objects)
+            .filter(Objects.DepartmentID == dept_id)
+        )
+        dept = session.execute(
+            select(Departments).where(Departments.DepartmentID == dept_id)
+        ).one()
+        if row_count > 0:
+            print(f"'{dept[0].Department}' ({dept_id}) has {row_count} objects.")
+
+
+if __name__ == "__main__":
+    engine = get_engine("data/private/tms_2024-08-07.db")
+    temp(engine)
+    # for i in range(0, 53):
+    #     try:
+    #         object_by_dept_count(engine, i)
+    #     except NoResultFound:
+    #         pass
