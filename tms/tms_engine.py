7f653a5a54796a2c7d29bb641c8f079762fd979f
Tomek <klingaroo@gmail.com>
2024-09-05 15:53:48 -0400

    add session

--- tms/tms_engine.py
+++ tms/tms_engine.py
@@ -1,6 +1,12 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 
 
 def get_engine(fh: str):
     engine = create_engine(f"sqlite:///{fh}")
     return engine
+
+
+def get_session(engine):
+    with Session(engine) as sess:
+        return sess
