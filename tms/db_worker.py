4e6a97862486fac77c62837e10761a45491a6c2e
Tomek <klingaroo@gmail.com>
2024-09-11 16:49:21 -0400

    renamed db_session to tms_db_conn

--- tms/db_worker.py
+++ tms/db_worker.py
@@ -2,8 +2,9 @@ from sqlalchemy import select, func
 from sqlalchemy.exc import NoResultFound
 from sqlalchemy.orm import Session
 
-from tms.db_session import get_tms_engine, get_manipulation_engine
-from tms.tms_models import Objects, Departments, Roles, RolesTypes
+from tms.tms_db_conn import get_tms_engine, get_manipulation_engine
+from tms.tms_models import Objects, ObjTitles, Departments, Roles, RolesTypes
+from tms.tms_values import TitleTypes
 
 
 def temp(engine):
@@ -29,6 +30,19 @@ def object_by_dept_count(engine, dept_id: int):
             print(f"'{dept[0].Department}' ({dept_id}) has {row_count} objects.")
 
 
+def title_types_in_ObjTitles_tbl(engine):
+    with Session(engine) as session:
+        for title, titletype_id in TitleTypes.__members__.items():
+            row_count = session.scalar(
+                select(func.count())
+                .select_from(ObjTitles)
+                .filter(ObjTitles.TitleTypeID == titletype_id)
+            )
+
+            # if row_count > 0:
+            print(f"{title} has {row_count} entries.")
+
+
 if __name__ == "__main__":
     engine = get_tms_engine()
-    temp(engine)
+    title_types_in_ObjectTitles_tbl(engine)
